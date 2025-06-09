df3d771645955210d69e0b5398deea898a9b78df
Mark Hallett <markhallett5@outlook.com>
2025-06-09 22:51:27 +0100

    Added comments to models.py

--- bookings/models.py
+++ bookings/models.py
@@ -8,7 +8,7 @@ class Walk(models.Model):
         ('MORNING', 'Morning'),
         ('AFTERNOON', 'Afternoon'),
     ]
-    walker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    walker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # Dog Walker
     date = models.DateField()
     time_of_day = models.CharField(max_length=10, choices=TIME_CHOICES)
     number_of_dogs = models.PositiveSmallIntegerField()
@@ -20,7 +20,7 @@ class Walk(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        unique_together = ['walker', 'date', 'time_of_day']
+        unique_together = ['walker', 'date', 'time_of_day'] # No double bookings for the walker at the same time & date
 
     def __str__(self):
         return f"Walk on {self.date} with {self.number_of_dogs} dog(s)"
