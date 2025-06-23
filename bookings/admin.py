bb7a7af13d3ff96da9ed6acf28dc2fd7824f70d9
Mark Hallett <markhallett5@outlook.com>
2025-06-23 22:09:04 +0100

    Add WalkAdmin class to customize Walk model in admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Walk, Booking, Review
 
 # Register your models here.
+
+class WalkAdmin(admin.ModelAdmin):
+    list_display = ('walker', 'date', 'time_of_day', 'number_of_dogs', 'address', 'created_at')
+    search_fields = ('walker__username', 'date', 'time_of_day', 'address')
+    list_filter = ('date', 'time_of_day', 'walker')
+
+
+
+
+admin.site.register(Walk, WalkAdmin)
+admin.site.register(Booking)
+admin.site.register(Review)
