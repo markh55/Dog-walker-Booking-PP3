0a44e23ed29774c16e014602b2b5f6a178e23a64
Mark Hallett <markhallett5@outlook.com>
2025-06-24 16:50:44 +0100

    installed Django Auth to handle my user login

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
 # path('', views.WalkListView.as_view(), name='walk-list'),
+    path("accounts/", include("allauth.urls")),
     path('', views.booking_list, name='booking-list'),
     path('create-booking/', views.BookingCreateView.as_view(), name='booking-create'),
 ]
--- dogwalkerbooking/settings.py
+++ dogwalkerbooking/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'bookings',  # Custom app for bookings
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'dogwalkerbooking.urls'
@@ -100,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
