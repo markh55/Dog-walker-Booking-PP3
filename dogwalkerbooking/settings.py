99d7bf11ac493af6766746aba212aa586a84f06e
Mark Hallett <markhallett5@outlook.com>
2025-06-24 19:46:59 +0100

    set up allauth aswell as setting up static file for css and styling

--- dogwalkerbooking/settings.py
+++ dogwalkerbooking/settings.py
@@ -133,3 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',  # Default backend
+]
+
+SOCIALACCOUNT_PROVIDERS = {}
