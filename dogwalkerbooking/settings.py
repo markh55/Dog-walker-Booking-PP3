b7f9a3843ddc07e238a457b0247612302e5366e8
Mark Hallett <markhallett5@outlook.com>
2025-06-28 22:18:29 +0100

    changed background colour to beige aswell as added navbar-custom

--- dogwalkerbooking/settings.py
+++ dogwalkerbooking/settings.py
@@ -24,7 +24,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-default-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
